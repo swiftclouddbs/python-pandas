b9e7f3c2e111aada13a588803353e95c7bd3843f
Reggie Stuart <rstuar1@yahoo.com>
2022-05-23 11:13:56 -0400

    Add files via upload

---
+++ AvgCostPerSqFt.py
@@ -0,0 +1,27 @@
+#This part averages the cost per sq ft
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import sys
+import pandas as pd
+
+UnionSquare = pd.read_csv('/home/dba/realestate/UnionSquareMA-trimmed-C.csv')
+
+UnionSquare.head()
+
+UnionSquare.SoldPrice = UnionSquare.SoldPrice.str.replace('$', '')
+UnionSquare.SoldPrice = UnionSquare.SoldPrice.str.replace(',', '')
+
+note = pd.to_numeric(UnionSquare.SoldPrice)
+
+note2 = UnionSquare.head()
+
+#print(note)
+#print(note2)
+
+UnionSquare['SoldPrice'] = UnionSquare['SoldPrice'].astype(float)
+a=UnionSquare['SoldPrice'].sum()
+b=UnionSquare['InteriorSqFt'].sum()
+
+AvgCostPerSqFt = str(round(a/b, 2))
+
+print('AverageCostPerSqFt is $',AvgCostPerSqFt)
